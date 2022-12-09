ddd2155a24acffac174c61f84b701c5fe98868c2
Guilherme <guilhermemleitte@hotmail.com>
2022-12-08 22:22:42 -0300

    Remoção de redundância

--- camera.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-camera.start_preview()
-sleep(5)
-camera.rotation = 180
-camera.capture('/home/pi/Desktop/image_3.jpg')
-camera.stop_preview()
